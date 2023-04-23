fe421570b784174e34f05823bd1fcb38793521aa
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-04-23 22:29:44 +0900

    最初の画面http://localhost:3000/usersが使えない

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  
+  resources :profiles
   devise_for :users
+  resources :users
   resources :posts
   root "posts#index"
+  get   "posts/index"
+
+  
+
+  get '/users', to: 'users#index'
   resource :account, only: [:edit, :update]
   get '/account', to: 'accounts#show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :profile,only: %i[show edit update]
+  patch 'profile', to: 'profiles#show'
+  resources :users, only: [:show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_073415) do
+ActiveRecord::Schema.define(version: 2023_04_23_014014) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -20,17 +20,12 @@ ActiveRecord::Schema.define(version: 2023_04_21_073415) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'avatar' for column 'string'
 
 end
